6b474ab2f0c3ef02154b170ff525893049b98ac1
Fekade Senbeta <fekadesenbeta@gmail.com>
2020-04-26 10:41:02 -0600

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,33 @@
 class Gallery
 
   attr_reader :name, :city
+  @@all = []
+
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.cities_with_gallery
+    cities_galleries = self.all.map do |gallery|
+      gallery.city
+    end
+    cities_galleries.uniq
+  end
+  
+  def get_artists
+    paintings = Painting.all.select do |painting|
+      painting.gallery == self
+    end
+    paintings.map do |painting|
+      painting.artist
+    end
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,22 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :style, :gallery
 
   @@all = []
 
   def initialize(title, price)
     @title = title
     @price = price
+    @style = style
+    @gallery = gallery
+    @@all << self
+  end
+
+  def self.styles
+    painting_styles = self.all.map do |painting|
+      painting.style
+    end
+    painting_styles.uniq
   end
 
 end
